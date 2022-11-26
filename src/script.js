95be33177f250a4c028356104d31d25f60635799
nishilbalmoori <80184797+nishilbalmoori@users.noreply.github.com>
2022-11-25 18:36:45 -0600

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,15 @@
+let canvas;
+let sprite;
+
+function setup(){
+    canvas = createCanvas(400,400);
+    sprite = new Sprite(100,100);
+}
+function draw(){
+    background(225);
+    // noStroke();
+    // fill(0,255,0);
+    // rect(100,100,50,100);
+
+
+}
